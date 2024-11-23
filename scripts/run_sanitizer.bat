77d746878604b9ffcdc46d47fd0a23dadf6230b4
vergelli <federico_joelvergelli@hotmail.com>
2024-11-23 20:18:07 -0300

    se agrega flag para mayor informacion de debugueo al sanitizer

--- scripts/run_sanitizer.bat
+++ scripts/run_sanitizer.bat
@@ -12,16 +12,16 @@ if "%1"=="" (
 REM Selecciona el comando basado en el argumento
 if "%1"=="memcheck" (
     echo Ejecutando memcheck...
-    compute-sanitizer --tool memcheck %OBJ_DIR%\cuNBSim.exe
+    compute-sanitizer --tool memcheck --target-processes all %OBJ_DIR%\cuNBSim.exe
 ) else if "%1"=="racecheck" (
     echo Ejecutando racecheck...
-    compute-sanitizer --tool racecheck %OBJ_DIR%\cuNBSim.exe
+    compute-sanitizer --tool racecheck --target-processes all %OBJ_DIR%\cuNBSim.exe
 ) else if "%1"=="initcheck" (
     echo Ejecutando initcheck...
-    compute-sanitizer --tool initcheck %OBJ_DIR%\cuNBSim.exe
+    compute-sanitizer --tool initcheck --target-processes all %OBJ_DIR%\cuNBSim.exe
 ) else if "%1"=="synccheck" (
     echo Ejecutando synccheck...
-    compute-sanitizer --tool synccheck %OBJ_DIR%\cuNBSim.exe
+    compute-sanitizer --tool synccheck --target-processes all %OBJ_DIR%\cuNBSim.exe
 ) else (
     echo Argumento no valido. Uso: run_profiling.bat [basic|complete]
 )
