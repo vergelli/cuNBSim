e61d8e0dc5c555f00523bd5a4a4db376a1d5c56e
vergelli <federico_joelvergelli@hotmail.com>
2024-11-30 11:01:07 -0300

    feat(25): Modificaciones pertinentes en entry point.

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 #include <cstdlib> 
 #include "body.cuh"
 #include "config.hpp"
@@ -8,10 +8,19 @@
 #include "deviceProps.cuh"
 #include "data_collector.cuh"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    //~ Si se pasa un argumento, lo usamos como path de la configuración
+    std::string config_path = (argc > 1) ? argv[1] : default_config_path;
+
+    //~ Cargar la configuración desde el archivo (usando el path que corresponda)
+    load_config_from_file(config_path);
 
     int bytes = nBodies * sizeof(Body);
-    printf("%d Bytes for %d particles\n", bytes, nBodies);
+    printf("INFO - Configuration file path: %s \n", config_path.c_str());
+    printf("INFO - %d Bytes for %d particles\n", bytes, nBodies);
+    printf("INFO - Iterations for simulation = %d and dt = %f \n", nIters, dt);
+
     float *buf;
     buf = (float *)malloc(bytes);
     Body *p = (Body*)buf;
@@ -48,9 +57,9 @@ int main() {
             iter);
     }
 
-    printf("Simulation terminated\n");
+    printf("INFO - Simulation terminated\n");
 
     //~ Rutinas de liberacion de memoria
-    cudaFreeMemRoutines(p_device);
+    cudaFreeMemRoutines(p_device, buf);
     return 0;
 }
