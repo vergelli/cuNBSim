b19da18eb5f76e3101f6ce5e53685bca81aa5c84
vergelli <federico_joelvergelli@hotmail.com>
2024-12-22 17:46:59 -0300

    refactor(28): Se traslada responsabilidad de integracion numerica usando Euler explicito hacia el Kernel de integracion pertinente.

--- kernels/integrate.cu
+++ kernels/integrate.cu
@@ -6,6 +6,10 @@ __global__ void integrateCUDA(Body *p_device, float dt, int nBody, int stride)
   int index = threadIdx.x + blockIdx.x * blockDim.x;
   for(int i = index; i<nBody ; i+=stride)
   {
+      p_device[i].vx += dt * p_device[i].fx; //Integrar velocidades con las fuerzas calculadas.
+      p_device[i].vy += dt * p_device[i].fy;
+      p_device[i].vz += dt * p_device[i].fz;
+
       p_device[i].x += p_device[i].vx*dt;
       p_device[i].y += p_device[i].vy*dt;
       p_device[i].z += p_device[i].vz*dt;
