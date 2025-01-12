41cd486b0f2e91ad15e859e690e339b121d5b936
vergelli <federico_joelvergelli@hotmail.com>
2025-01-12 20:39:34 -0300

    feat(56): Refactor configuration handling and enhance load_config_from_file function

--- include/config.hpp
+++ include/config.hpp
@@ -2,14 +2,33 @@
 #define CONFIG_HPP
 #include <string> 
 
-extern float dt;
+extern const std::string default_config_path;
+
+extern std::string data_directory;
+extern std::string simulation_data_file_name;
+extern std::string position_initializer;
+extern std::string numerical_integrator;
 extern int nIters;
 extern int nBodies;
+extern int gridDimX;
+extern int blockDimX;
+extern int gridDimY;
+extern int blockDimY;
+extern int gridDimZ;
+extern int blockDimZ;
+extern int integrateStride;
+extern float position_std_dev_x;
+extern float position_std_dev_y;
+extern float position_std_dev_z;
+extern float pi_value;
+extern float dt;
 extern float max_particles_speed;
-extern const std::string data_directory;
-extern const std::string default_config_path;
-extern const std::string simulation_data_file_name;
+extern float G;
+extern float SOFTENING;
+extern float MIN_DISTANCE_THRESHOLD;
+extern float MASS_SOFTENING;
+extern bool launch_params_automatic;
 
-void load_config_from_file(const std::string& config_file);
+void load_config_from_file(const std::string& config_file, DeviceProperties deviceProps);
 
 #endif // CONFIG_HPP
--- src/host/config.cpp
+++ src/host/config.cpp
@@ -1,44 +1,107 @@
 #include "config.hpp"
 #include "validation.hpp"
 #include "json.hpp"
+#include "deviceProps.cuh"
 #include <iostream>
 #include <fstream>
 #include <string> 
 
-//~ Definir las variables globales aquí (una única vez)
-const  std::string data_directory = "../data/";
-const  std::string simulation_data_file_name = "simulation_data.csv";
-const  std::string default_config_path = "../config/config.json";
+void initialize_default_values() {
 
-float dt = 0.001f;
-float max_particles_speed = 1;
-int nBodies = 256;
-int nIters = 10;
+    std::cout << "INFO - Initializing default variables" << std::endl;
 
+    const  std::string default_config_path = "../config/config.json";
+    data_directory = "../data/";
+    simulation_data_file_name = "simulation_data.csv";
+    std::cout << "INFO - Default values initialized:" << std::endl;
+    std::cout << "INFO - data_directory: " << data_directory << std::endl;
+    std::cout << "INFO - simulation_data_file_name: " << simulation_data_file_name << std::endl;
+}
 
-void load_config_from_file(const std::string& config_file) {
+void initialize_configuration_variables(const nlohmann::json& config) {
+
+    std::cout << "INFO - Initializing configuration variables" << std::endl;
+
+    nBodies = config["simulation"]["nBodies"];
+    nIters = config["simulation"]["nIters"];
+    dt = config["simulation"]["dt"];
+    //* position_initializer ------------------------------------------
+    for (const auto& item : config["simulation"]["position"].items()) {
+        if (item.value().contains("on") && item.value()["on"]) {
+            position_initializer = item.key();
+            break;
+        }
+    }
+    if (position_initializer == "box-muller") {
+        position_std_dev_x = config["simulation"]["position"]["box-muller"]["position_std_dev_x"];
+        position_std_dev_y = config["simulation"]["position"]["box-muller"]["position_std_dev_y"];
+        position_std_dev_z = config["simulation"]["position"]["box-muller"]["position_std_dev_z"];
+        pi_value = config["simulation"]["position"]["box-muller"]["pi_value"];
+    }
+    max_particles_speed = config["simulation"]["velocity"]["max_particles_speed"];
+    MASS_SOFTENING = config["simulation"]["mass"]["mass_softening"];
+    G = config["simulation"]["force"]["G"];
+    SOFTENING = config["simulation"]["force"]["SOFTENING"];
+    MIN_DISTANCE_THRESHOLD = config["simulation"]["force"]["MIN_DISTANCE_TRESHOLD"];
+    //* numeric integration ------------------------------------------
+    for (const auto& item : config["simulation"]["numeric-integration"].items()) {
+        if (item.value().contains("on") && item.value()["on"]) {
+            numerical_integrator = item.key();
+            break;
+        }
+    }
+    //* device --------------------------------------------------------
+    launch_params_automatic = config["device"]["launch-params-automatic"];
+    if (!launch_params_automatic) {
+        gridDimX = config["device"]["launch-params-manual"]["gridDimX"];
+        blockDimX = config["device"]["launch-params-manual"]["blockDimX"];
+        gridDimY = config["device"]["launch-params-manual"]["gridDimY"];
+        blockDimY = config["device"]["launch-params-manual"]["blockDimY"];
+        gridDimZ = config["device"]["launch-params-manual"]["gridDimZ"];
+        blockDimZ = config["device"]["launch-params-manual"]["blockDimZ"];
+        integrateStride = config["device"]["launch-params-manual"]["IntegrateStride"];
+    }
+
+    std::cout << "INFO - Configuration values initialized:" << std::endl;
+    std::cout << "INFO - nBodies: " << nBodies << std::endl;
+    std::cout << "INFO - nIters: " << nIters << std::endl;
+    std::cout << "INFO - dt: " << dt << std::endl;
+    std::cout << "INFO - position_initializer: " << position_initializer << std::endl;
+    if (position_initializer == "box-muller") {
+        std::cout << "INFO - position_std_dev_x: " << position_std_dev_x << std::endl;
+        std::cout << "INFO - position_std_dev_y: " << position_std_dev_y << std::endl;
+        std::cout << "INFO - position_std_dev_z: " << position_std_dev_z << std::endl;
+        std::cout << "INFO - pi_value: " << pi_value << std::endl;
+    }
+    std::cout << "INFO - max_particles_speed: " << max_particles_speed << std::endl;
+    std::cout << "INFO - MASS_SOFTENING: " << MASS_SOFTENING << std::endl;
+    std::cout << "INFO - G: " << G << std::endl;
+    std::cout << "INFO - SOFTENING: " << SOFTENING << std::endl;
+    std::cout << "INFO - MIN_DISTANCE_THRESHOLD: " << MIN_DISTANCE_THRESHOLD << std::endl;
+    std::cout << "INFO - numerical_integrator: " << numerical_integrator << std::endl;
+    std::cout << "INFO - launch_params_automatic: " << launch_params_automatic << std::endl;
+    if (!launch_params_automatic) {
+        std::cout << "INFO - gridDimX: " << gridDimX << std::endl;
+        std::cout << "INFO - blockDimX: " << blockDimX << std::endl;
+        std::cout << "INFO - gridDimY: " << gridDimY << std::endl;
+        std::cout << "INFO - blockDimY: " << blockDimY << std::endl;
+        std::cout << "INFO - gridDimZ: " << gridDimZ << std::endl;
+        std::cout << "INFO - blockDimZ: " << blockDimZ << std::endl;
+        std::cout << "INFO - integrateStride: " << integrateStride << std::endl;
+    }
+}
+
+void load_config_from_file(const std::string& config_file, DeviceProperties deviceProps) {
     std::ifstream file(config_file);
     if (!file.is_open()) {
         std::cerr << "ERROR - No se pudo abrir el archivo de configuración." << std::endl;
         return;
     }
-
     nlohmann::json config;
     file >> config;
+    config_file_validation_routines(config, deviceProps);
+    initialize_default_values();
+    initialize_configuration_variables(config);
+    std::cout << "INFO - Configuration file loaded." << std::endl;
 
-    config_file_validation_routines(config);
-
-    // Leer los valores desde el archivo JSON
-    if (config.contains("nBodies")) {
-        nBodies = config["nBodies"];
-    }
-    if (config.contains("nIters")) {
-        nIters = config["nIters"];
-    }
-    if (config.contains("dt")) {
-        dt = config["dt"];
-    }
-    if (config.contains("max_particles_speed")) {
-        max_particles_speed = config["max_particles_speed"];
-    }
 }
