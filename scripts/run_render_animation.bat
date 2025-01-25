8697d19671a71a178d0656c119d0bf6f8649ac49
vergelli <federico_joelvergelli@hotmail.com>
2025-01-25 17:08:52 -0300

    feat: Update batch script with comments for clarity on NBody simulation rendering

--- scripts/run_render_animation.bat
+++ scripts/run_render_animation.bat
@@ -1,3 +1,7 @@
+::! ESPERA MUCHOS CAMBIOS EN EL FUTURO !::
+REM Este script ejecuta el script de Julia que renderiza la animacion de la simulacion 
+REM NBody en sus proyecciones solamente
+
 @echo off
 echo Running renderNBodySimAnimation3.jl script with Julia...
 julia ..\postprocess\renderNBodySimAnimation3.jl
